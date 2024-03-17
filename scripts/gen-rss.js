d544305fc4cbe519d3857f60d6f9b56777ce449d
elMagoo <miguelapb35@gmail.com>
2024-03-16 21:03:12 -0500

    ok

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,7 +5,7 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
+    title: 'Interesting Feed',
     site_url: 'https://yoursite.com',
     feed_url: 'https://yoursite.com/feed.xml'
   })
